4785a6caa78a13f93d5753c8cbc1217c3da48cfc
Aninoss <54690141+Aninoss@users.noreply.github.com>
2020-09-20 10:51:34 +0200

    Small improvements

--- src/main/java/commands/runnables/informationcategory/PatreonCommand.java
+++ src/main/java/commands/runnables/informationcategory/PatreonCommand.java
@@ -11,6 +11,7 @@ import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.entity.permission.Role;
 import org.javacord.api.event.message.MessageCreateEvent;
 
+import java.util.Arrays;
 import java.util.Locale;
 
 @CommandProperties(
@@ -20,6 +21,10 @@ import java.util.Locale;
         aliases = {"donate", "donation", "premium"}
 )
 public class PatreonCommand extends Command {
+
+    private final long[] USER_ID_NOT_VISIBLE = {
+            397209883793162240L
+    };
 
     public PatreonCommand(Locale locale, String prefix) {
         super(locale, prefix);
@@ -43,15 +48,17 @@ public class PatreonCommand extends Command {
     }
 
     private String getPatreonUsersString(int patreonTier) {
-        StringBuilder donators = new StringBuilder();
+        StringBuilder patreonUsers = new StringBuilder();
         Role role = DiscordApiCollection.getInstance().getServerById(Settings.SUPPORT_SERVER_ID).get().getRoleById(Settings.PATREON_ROLE_IDS[patreonTier]).get();
-        role.getUsers().forEach(user -> {
+        role.getUsers().stream()
+                .filter(user -> Arrays.stream(USER_ID_NOT_VISIBLE).noneMatch(userIdBlock -> user.getId() == userIdBlock))
+                .forEach(user -> {
             String value = getString("slot_value", patreonTier);
-            donators.append(getString("slot", StringUtil.escapeMarkdown(user.getDiscriminatedName()), value))
+            patreonUsers.append(getString("slot", StringUtil.escapeMarkdown(user.getDiscriminatedName()), value))
                     .append("\n");
         });
 
-        return donators.toString();
+        return patreonUsers.toString();
     }
 
 }
