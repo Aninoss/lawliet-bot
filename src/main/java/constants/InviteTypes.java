50a2a5f68e56ec356ae68ff08ebd0358f6549a4e
Aninoss <54690141+Aninoss@users.noreply.github.com>
2020-09-28 11:34:04 +0200

    New invite type for website on top.gg

--- src/main/java/constants/InviteTypes.java
+++ src/main/java/constants/InviteTypes.java
@@ -5,6 +5,8 @@ public enum InviteTypes {
     BOT,
     BOT_REMINDER,
     WEBSITE,
+    WEBSITE_DISCORD_SERVERS_ME,
+    WEBSITE_TOPGG,
     TOP_GG,
     BOTS_ONDISCORD_XYZ,
     DISCORD_BOTS_GG,
@@ -12,6 +14,5 @@ public enum InviteTypes {
     BOTSFORDISCORD_COM,
     DISCORD_BOATS,
     TOP_BOTS_XYZ,
-    WEBSITE_DISCORD_SERVERS_ME
 
 }
