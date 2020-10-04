b6b14259934e25ab3d08f7bbdc81c87bcab9d8de
Aninoss <54690141+Aninoss@users.noreply.github.com>
2020-10-04 19:02:28 +0200

    New gifs for "L.sip"

--- src/main/java/commands/runnables/emotescategory/SipCommand.java
+++ src/main/java/commands/runnables/emotescategory/SipCommand.java
@@ -28,7 +28,9 @@ public class SipCommand extends EmoteAbstract {
                 "https://media.discordapp.net/attachments/761612990784471041/761613511892795473/sip.gif",
                 "https://media.discordapp.net/attachments/761612990784471041/761613569317666886/sip.gif",
                 "https://media.discordapp.net/attachments/761612990784471041/761620581787238410/sip.gif",
-                "https://media.discordapp.net/attachments/761612990784471041/761613660744712252/sip.gif"
+                "https://media.discordapp.net/attachments/761612990784471041/761613660744712252/sip.gif",
+                "https://media.discordapp.net/attachments/761612990784471041/762355934864474112/sip.gif",
+                "https://media.discordapp.net/attachments/761612990784471041/762358715964850176/sip.gif"
         };
     }
 
