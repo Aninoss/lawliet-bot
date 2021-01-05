21e694056988cfff12e5c2edc2666c902cb99395
Aninoss <leonblech@tutanota.com>
2021-01-05 14:29:16 +0100

    Minor improvements

--- src/main/java/core/GlobalThreadPool.java
+++ src/main/java/core/GlobalThreadPool.java
@@ -4,7 +4,9 @@ import java.util.concurrent.*;
 
 public class GlobalThreadPool {
 
-    private static final ExecutorService executorService = Executors.newFixedThreadPool(50);
+    private static final ExecutorService executorService = new ThreadPoolExecutor(0, 50,
+            60L, TimeUnit.SECONDS,
+            new SynchronousQueue<>());
 
     public static ExecutorService getExecutorService() {
         return executorService;
--- src/main/java/events/discordevents/messagecreate/MessageCreateSingleBotMention.java
+++ src/main/java/events/discordevents/messagecreate/MessageCreateSingleBotMention.java
@@ -10,21 +10,17 @@ import mysql.modules.server.DBServer;
 import mysql.modules.server.ServerBean;
 import org.javacord.api.event.message.MessageCreateEvent;
 
-@DiscordEvent(priority = EventPriority.HIGH)
+@DiscordEvent(priority = EventPriority.MEDIUM)
 public class MessageCreateSingleBotMention extends MessageCreateAbstract {
 
     @Override
     public boolean onMessageCreate(MessageCreateEvent event) throws Throwable {
-        ServerBean serverBean = DBServer.getInstance().getBean(event.getServer().get().getId());
-        String prefix = serverBean.getPrefix();
-        String content = event.getMessageContent();
+        if (StringUtil.trimString(event.getMessageContent().replace("@!", "@")).equalsIgnoreCase(DiscordApiManager.getInstance().getYourself().getMentionTag())) {
+            ServerBean serverBean = DBServer.getInstance().getBean(event.getServer().get().getId());
 
-        if (content.equalsIgnoreCase("i.") && prefix.equalsIgnoreCase("L."))
-            content = prefix;
-
-        if (prefix.equalsIgnoreCase(content) || StringUtil.trimString(event.getMessageContent().replace("@!", "@")).equalsIgnoreCase(DiscordApiManager.getInstance().getYourself().getMentionTag())) {
             String text = TextManager.getString(serverBean.getLocale(), TextManager.GENERAL, "bot_ping_help", serverBean.getPrefix());
-            if (event.getChannel().canYouWrite()) event.getChannel().sendMessage(text).get();
+            if (event.getChannel().canYouWrite())
+                event.getChannel().sendMessage(text).get();
 
             return false;
         }
