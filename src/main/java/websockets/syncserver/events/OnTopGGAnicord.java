b39fa3f2d261faad727230f7e6f1f56171ac28a9
Aninoss <leonblech@tutanota.com>
2021-06-09 00:25:40 +0200

    Bug fix

--- src/main/java/websockets/syncserver/events/OnTopGGAnicord.java
+++ src/main/java/websockets/syncserver/events/OnTopGGAnicord.java
@@ -1,12 +1,10 @@
 package websockets.syncserver.events;
 
-import java.util.Locale;
+import java.text.MessageFormat;
 import java.util.Optional;
 import constants.AssetIds;
-import constants.Language;
 import core.EmbedFactory;
 import core.ShardManager;
-import core.TextManager;
 import core.utils.StringUtil;
 import modules.Fishery;
 import mysql.modules.fisheryusers.DBFishery;
@@ -21,14 +19,13 @@ public class OnTopGGAnicord extends OnTopGG {
     @Override
     protected void processUpvote(long userId, boolean isWeekend) {
         Guild guild = ShardManager.getInstance().getLocalGuildById(AssetIds.ANICORD_SERVER_ID).get();
-        Locale locale = Language.DE.getLocale();
         Optional.ofNullable(guild.getMemberById(userId)).ifPresent(user -> {
             TextChannel bumpChannel = guild.getTextChannelById(713849992611102781L);
 
             FisheryMemberData userBean = DBFishery.getInstance().retrieve(guild.getIdLong()).getMemberBean(userId);
             long add = Fishery.getClaimValue(userBean);
 
-            String desc = TextManager.getString(locale, TextManager.GENERAL, "topgg_aninoss", user.getAsMention(), guild.getName(), StringUtil.numToString(add), String.format("https://top.gg/servers/%d/vote", AssetIds.ANICORD_SERVER_ID));
+            String desc = MessageFormat.format("✅ | {0} hat auf [top.gg]({3}) für **{1}** geupvotet und dafür **🐟 {2}** (25% der Daily-Fische) erhalten!", user.getAsMention(), guild.getName(), StringUtil.numToString(add), String.format("https://top.gg/servers/%d/vote", AssetIds.ANICORD_SERVER_ID));
             bumpChannel.sendMessage(EmbedFactory.getEmbedDefault().setDescription(desc).build()).queue();
             bumpChannel.sendMessage(userBean.changeValuesEmbed(add, 0).build()).queue();
         });
