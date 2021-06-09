b5d869fa40919c87dec65e081f6696e34cbb3906
Aninoss <leonblech@tutanota.com>
2021-06-09 09:50:53 +0200

    Lawliet DM with button

--- src/main/java/events/discordevents/privatemessagereceived/PrivateMessageReceivedDMResponse.java
+++ src/main/java/events/discordevents/privatemessagereceived/PrivateMessageReceivedDMResponse.java
@@ -1,16 +1,22 @@
 package events.discordevents.privatemessagereceived;
 
 import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import constants.ExternalLinks;
-import core.Program;
 import core.EmbedFactory;
+import core.Program;
+import core.components.ActionRows;
 import events.discordevents.DiscordEvent;
 import events.discordevents.eventtypeabstracts.PrivateMessageReceivedAbstract;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.Button;
+import net.dv8tion.jda.api.interactions.components.ButtonStyle;
 
 @DiscordEvent
 public class PrivateMessageReceivedDMResponse extends PrivateMessageReceivedAbstract {
@@ -25,16 +31,21 @@ public class PrivateMessageReceivedDMResponse extends PrivateMessageReceivedAbst
         if (Program.getClusterId() == 1 && !usersDmNotified.asMap().containsKey(user.getIdLong())) {
             usersDmNotified.put(user.getIdLong(), true);
             EmbedBuilder eb;
+            List<ActionRow> actionRowList = Collections.emptyList();
             if (Program.isPublicVersion()) {
                 eb = EmbedFactory.getEmbedError()
                         .setTitle("❌ Not Supported")
-                        .setDescription(String.format("Commands via dm are not supported, you need to [invite](%s) Lawliet into a server!", ExternalLinks.BOT_INVITE_URL));
+                        .setDescription("Commands via dm are not supported, you need to invite Lawliet into a server!");
+                actionRowList = ActionRows.of(Button.of(ButtonStyle.LINK, ExternalLinks.BOT_INVITE_URL, "Invite Lawliet"));
             } else {
                 eb = EmbedFactory.getEmbedError()
                         .setTitle("❌ Not Supported")
                         .setDescription("Commands via dm are not supported!");
             }
-            event.getMessage().reply(eb.build()).queue();
+            event.getMessage()
+                    .reply(eb.build())
+                    .setActionRows(actionRowList)
+                    .queue();
         }
 
         return true;
