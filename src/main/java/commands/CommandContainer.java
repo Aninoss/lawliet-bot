af3c07fc316b69f19ceebb8afb60864be7092315
Aninoss <leonblech@tutanota.com>
2021-09-06 20:49:58 +0200

    New role play command L.footjob

--- src/main/java/commands/CommandContainer.java
+++ src/main/java/commands/CommandContainer.java
@@ -217,6 +217,7 @@ public class CommandContainer {
         commandList.add(BlowjobCommand.class);
         commandList.add(BoobsGrabCommand.class);
         commandList.add(CumCommand.class);
+        commandList.add(FootjobCommand.class);
         commandList.add(FuckCommand.class);
         commandList.add(FurryFuckCommand.class);
         commandList.add(MasturbateCommand.class);
---
+++ src/main/java/commands/runnables/interactionscategory/FootjobCommand.java
@@ -0,0 +1,32 @@
+package commands.runnables.interactionscategory;
+
+import java.util.Locale;
+import commands.listeners.CommandProperties;
+import commands.runnables.RolePlayAbstract;
+
+@CommandProperties(
+        trigger = "footjob",
+        emoji = "🦶",
+        executableWithoutArgs = true,
+        nsfw = true,
+        requiresFullMemberCache = true,
+        aliases = { "feetjob", "feet" }
+)
+public class FootjobCommand extends RolePlayAbstract {
+
+    public FootjobCommand(Locale locale, String prefix) {
+        super(locale, prefix, true,
+                "https://cdn.discordapp.com/attachments/884506240179372112/884506336732278844/footjob.gif",
+                "https://cdn.discordapp.com/attachments/884506240179372112/884506467409989642/footjob.gif",
+                "https://cdn.discordapp.com/attachments/884506240179372112/884506586196885604/footjob.gif",
+                "https://cdn.discordapp.com/attachments/884506240179372112/884506706317570079/footjob.gif",
+                "https://cdn.discordapp.com/attachments/884506240179372112/884506781299126302/footjob.gif",
+                "https://cdn.discordapp.com/attachments/884506240179372112/884506860911231016/footjob.gif",
+                "https://cdn.discordapp.com/attachments/884506240179372112/884507081099608134/footjob.gif",
+                "https://cdn.discordapp.com/attachments/884506240179372112/884507187127410708/footjob.gif",
+                "https://cdn.discordapp.com/attachments/884506240179372112/884507241447837748/footjob.gif",
+                "https://cdn.discordapp.com/attachments/884506240179372112/884507559057317938/footjob.gif"
+        );
+    }
+
+}
