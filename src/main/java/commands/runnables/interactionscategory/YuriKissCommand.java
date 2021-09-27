3913ec1170413ab379cadcdcbf973ad80f1882c5
Aninoss <leonblech@tutanota.com>
2021-09-27 23:50:48 +0200

    New yuri kiss gif

--- src/main/java/commands/runnables/interactionscategory/YuriKissCommand.java
+++ src/main/java/commands/runnables/interactionscategory/YuriKissCommand.java
@@ -31,7 +31,8 @@ public class YuriKissCommand extends RolePlayAbstract {
                 "https://cdn.discordapp.com/attachments/736281091534618674/834849805246660608/yurikiss.gif",
                 "https://cdn.discordapp.com/attachments/736281091534618674/834849818278887435/yurikiss.gif",
                 "https://cdn.discordapp.com/attachments/736281091534618674/834849832022704158/yurikiss.gif",
-                "https://cdn.discordapp.com/attachments/736281091534618674/863573727069077545/yurikiss.gif"
+                "https://cdn.discordapp.com/attachments/736281091534618674/863573727069077545/yurikiss.gif",
+                "https://cdn.discordapp.com/attachments/736281091534618674/892166330340499506/yurikiss.gif"
         );
     }
 
