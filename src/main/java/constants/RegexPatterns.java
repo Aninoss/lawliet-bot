8a164105127933915a748a85200b431498a5f764
Aninoss <leonblech@tutanota.com>
2021-10-12 22:32:51 +0200

    Nsfw counter regex fix

--- src/main/java/constants/RegexPatterns.java
+++ src/main/java/constants/RegexPatterns.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public interface RegexPatterns {
 
-    Pattern BOORU_AMOUNT_PATTERN = Pattern.compile("\\b\\d{1,6}\\b");
+    Pattern BOORU_AMOUNT_PATTERN = Pattern.compile("(?<![=><])\\b\\d{1,6}\\b");
     Pattern DIGIT_REFORMAT_PATTERN = Pattern.compile("\\b[0-9]+[\\s| ]+[0-9]");
     Pattern AMOUNT_FILTER_PATTERN = Pattern.compile("\\b(?<digits>\\d{1,16}([.|,]\\d{1,16})?)[\\s| ]*(?<unit>[\\w|%]*)");
     Pattern MINUTES_PATTERN = Pattern.compile("\\b\\d{1,4}[\\s| ]*m(in(utes?)?)?\\b");
