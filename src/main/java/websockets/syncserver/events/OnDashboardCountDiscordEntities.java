73efd294ddc4b825abd04d173aede469a5dbdb58
Aninoss <leonblech@tutanota.com>
2021-12-20 01:49:59 +0100

    Discord list and count events

---
+++ src/main/java/websockets/syncserver/events/OnDashboardCountDiscordEntities.java
@@ -0,0 +1,52 @@
+package websockets.syncserver.events;
+
+import core.MemberCacheController;
+import core.ShardManager;
+import core.utils.BotPermissionUtil;
+import dashboard.component.DashboardDiscordEntitySelection;
+import net.dv8tion.jda.api.entities.Member;
+import org.json.JSONObject;
+import websockets.syncserver.SyncServerEvent;
+import websockets.syncserver.SyncServerFunction;
+
+@SyncServerEvent(event = "DASH_COUNT_DISCORD_ENTITIES")
+public class OnDashboardCountDiscordEntities implements SyncServerFunction {
+
+    @Override
+    public JSONObject apply(JSONObject jsonObject) {
+        JSONObject resultJson = new JSONObject();
+
+        long userId = jsonObject.getLong("user_id");
+        long guildId = jsonObject.getLong("guild_id");
+        DashboardDiscordEntitySelection.DataType type = DashboardDiscordEntitySelection.DataType.valueOf(jsonObject.getString("type"));
+        String filterText = jsonObject.getString("filter_text").toLowerCase();
+
+        long count = ShardManager.getLocalGuildById(guildId).map(guild -> switch (type) {
+            case MEMBERS -> MemberCacheController.getInstance().loadMembersFull(guild).join().stream()
+                    .filter(m -> m.getUser().getAsTag().toLowerCase().contains(filterText))
+                    .count();
+
+            case ROLES -> guild.getRoles().stream()
+                    .filter(r -> r.getName().toLowerCase().contains(filterText) && !r.isPublicRole() && !r.isManaged())
+                    .count();
+
+            case TEXT_CHANNELS -> {
+                Member member = MemberCacheController.getInstance().loadMember(guild, userId).join();
+                yield guild.getTextChannels().stream()
+                        .filter(c -> ("#" + c.getName().toLowerCase()).contains(filterText) && BotPermissionUtil.can(c) && BotPermissionUtil.can(member, c))
+                        .count();
+            }
+
+            case VOICE_CHANNELS -> {
+                Member member = MemberCacheController.getInstance().loadMember(guild, userId).join();
+                yield guild.getVoiceChannels().stream()
+                        .filter(c -> c.getName().toLowerCase().contains(filterText) && BotPermissionUtil.can(c) && BotPermissionUtil.can(member, c))
+                        .count();
+            }
+        }).orElse(0L);
+
+        resultJson.put("count", count);
+        return resultJson;
+    }
+
+}
---
+++ src/main/java/websockets/syncserver/events/OnDashboardListDiscordEntities.java
@@ -0,0 +1,86 @@
+package websockets.syncserver.events;
+
+import core.MemberCacheController;
+import core.ShardManager;
+import core.utils.BotPermissionUtil;
+import dashboard.component.DashboardDiscordEntitySelection;
+import net.dv8tion.jda.api.entities.Member;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import websockets.syncserver.SyncServerEvent;
+import websockets.syncserver.SyncServerFunction;
+
+@SyncServerEvent(event = "DASH_LIST_DISCORD_ENTITIES")
+public class OnDashboardListDiscordEntities implements SyncServerFunction {
+
+    @Override
+    public JSONObject apply(JSONObject jsonObject) {
+        JSONObject resultJson = new JSONObject();
+        JSONArray entitiesJson = new JSONArray();
+
+        long userId = jsonObject.getLong("user_id");
+        long guildId = jsonObject.getLong("guild_id");
+        DashboardDiscordEntitySelection.DataType type = DashboardDiscordEntitySelection.DataType.valueOf(jsonObject.getString("type"));
+        String filterText = jsonObject.getString("filter_text").toLowerCase();
+        int offset = jsonObject.getInt("offset");
+        int limit = jsonObject.getInt("limit");
+
+        ShardManager.getLocalGuildById(guildId).ifPresent(guild -> {
+            switch (type) {
+                case MEMBERS -> MemberCacheController.getInstance().loadMembersFull(guild).join().stream()
+                        .filter(m -> m.getUser().getAsTag().toLowerCase().contains(filterText))
+                        .skip(offset)
+                        .limit(limit)
+                        .forEach(m -> {
+                            JSONObject json = new JSONObject();
+                            json.put("id", m.getIdLong());
+                            json.put("name", m.getUser().getAsTag());
+                            entitiesJson.put(json);
+                        });
+
+                case ROLES -> guild.getRoles().stream()
+                        .filter(r -> r.getName().toLowerCase().contains(filterText) && !r.isPublicRole() && !r.isManaged())
+                        .skip(offset)
+                        .limit(limit)
+                        .forEach(r -> {
+                            JSONObject json = new JSONObject();
+                            json.put("id", r.getIdLong());
+                            json.put("name", r.getName());
+                            entitiesJson.put(json);
+                        });
+
+                case TEXT_CHANNELS -> {
+                    Member member = MemberCacheController.getInstance().loadMember(guild, userId).join();
+                    guild.getTextChannels().stream()
+                            .filter(c -> ("#" + c.getName().toLowerCase()).contains(filterText) && BotPermissionUtil.can(c) && BotPermissionUtil.can(member, c))
+                            .skip(offset)
+                            .limit(limit)
+                            .forEach(c -> {
+                                JSONObject json = new JSONObject();
+                                json.put("id", c.getIdLong());
+                                json.put("name", "#" + c.getName());
+                                entitiesJson.put(json);
+                            });
+                }
+
+                case VOICE_CHANNELS -> {
+                    Member member = MemberCacheController.getInstance().loadMember(guild, userId).join();
+                    guild.getVoiceChannels().stream()
+                            .filter(c -> c.getName().toLowerCase().contains(filterText) && BotPermissionUtil.can(c) && BotPermissionUtil.can(member, c))
+                            .skip(offset)
+                            .limit(limit)
+                            .forEach(c -> {
+                                JSONObject json = new JSONObject();
+                                json.put("id", c.getIdLong());
+                                json.put("name", c.getName());
+                                entitiesJson.put(json);
+                            });
+                }
+            }
+        });
+
+        resultJson.put("entities", entitiesJson);
+        return resultJson;
+    }
+
+}
