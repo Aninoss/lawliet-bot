10bd8d5d7b5ede006eba9c3d1c9ff8d23c8679df
Aninoss <leonblech@tutanota.com>
2021-12-29 18:41:40 +0100

    New gif for L.punch

--- src/main/java/commands/runnables/interactionscategory/PunchCommand.java
+++ src/main/java/commands/runnables/interactionscategory/PunchCommand.java
@@ -55,7 +55,8 @@ public class PunchCommand extends RolePlayAbstract {
                 "https://cdn.discordapp.com/attachments/736270998772383826/834837260552896612/punch.gif",
                 "https://cdn.discordapp.com/attachments/736270998772383826/834837274155155546/punch.gif",
                 "https://cdn.discordapp.com/attachments/736270998772383826/871035786491985950/punch.gif",
-                "https://cdn.discordapp.com/attachments/736270998772383826/890291777133772840/punch.gif"
+                "https://cdn.discordapp.com/attachments/736270998772383826/890291777133772840/punch.gif",
+                "https://cdn.discordapp.com/attachments/736270998772383826/925805711265497088/punch.gif"
         );
     }
 
