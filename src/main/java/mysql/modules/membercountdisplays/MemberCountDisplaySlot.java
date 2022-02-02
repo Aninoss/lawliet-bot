e6eee797b64e2674c2aaefcd091193d13a4b91c7
Aninoss <leonblech@tutanota.com>
2022-02-02 19:05:04 +0100

    Small improvement

--- src/main/java/mysql/modules/membercountdisplays/MemberCountDisplaySlot.java
+++ src/main/java/mysql/modules/membercountdisplays/MemberCountDisplaySlot.java
@@ -1,6 +1,7 @@
 package mysql.modules.membercountdisplays;
 
 import core.assets.VoiceChannelAsset;
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class MemberCountDisplaySlot implements VoiceChannelAsset {
 
@@ -8,7 +9,7 @@ public class MemberCountDisplaySlot implements VoiceChannelAsset {
     private final long voiceChannelId;
     private final String mask;
 
-    public MemberCountDisplaySlot(long guildId, long voiceChannelId, String mask) {
+    public MemberCountDisplaySlot(long guildId, long voiceChannelId, @NonNull String mask) {
         this.guildId = guildId;
         this.voiceChannelId = voiceChannelId;
         this.mask = mask;
