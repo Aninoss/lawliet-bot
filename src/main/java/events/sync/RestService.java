39a5cb8878818e54e9d19a2d6348d5eac79c8475
Aninoss <leonblech@tutanota.com>
2022-03-27 16:53:09 +0200

    Small fixes

--- src/main/java/events/sync/RestService.java
+++ src/main/java/events/sync/RestService.java
@@ -2,18 +2,15 @@ package events.sync;
 
 import java.time.Duration;
 import core.AsyncTimer;
+import core.MainLogger;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Path("")
 @Singleton
 public class RestService {
-
-    private final static Logger LOGGER = LoggerFactory.getLogger(RestService.class);
 
     @POST
     @Path("/{name}")
@@ -26,14 +23,14 @@ public class RestService {
             try {
                 responseJson = EventManager.getEvent(name).apply(requestJson);
             } catch (Throwable e) {
-                LOGGER.error("Error in event \"{}\"", name, e);
+                MainLogger.get().error("Error in event \"{}\"", name, e);
             }
             if (responseJson == null) {
                 responseJson = new JSONObject();
             }
             return responseJson.toString();
         } catch (Throwable e) {
-            LOGGER.error("Error in event \"{}\"", name, e);
+            MainLogger.get().error("Error in event \"{}\"", name, e);
             return new JSONObject().toString();
         }
     }
