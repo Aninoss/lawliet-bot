5bfd203cb1dff9db588ec5ac2339e3a946ec7111
aninoss <leonblech@tutanota.com>
2022-06-04 07:00:25 +0200

    L.prefix without args support

--- src/main/java/commands/runnables/configurationcategory/PrefixCommand.java
+++ src/main/java/commands/runnables/configurationcategory/PrefixCommand.java
@@ -4,20 +4,32 @@ import java.util.Locale;
 import commands.Command;
 import commands.CommandEvent;
 import commands.listeners.CommandProperties;
+import commands.listeners.OnButtonListener;
 import core.EmbedFactory;
 import core.ExceptionLogger;
+import core.ModalMediator;
 import core.TextManager;
+import core.utils.StringUtil;
 import modules.Prefix;
+import mysql.modules.guild.DBGuild;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.Modal;
+import net.dv8tion.jda.api.interactions.components.text.TextInput;
+import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import org.jetbrains.annotations.NotNull;
 
 @CommandProperties(
         trigger = "prefix",
         userGuildPermissions = Permission.MANAGE_SERVER,
         emoji = "\uD83D\uDCDB",
-        executableWithoutArgs = false
+        executableWithoutArgs = true
 )
-public class PrefixCommand extends Command {
+public class PrefixCommand extends Command implements OnButtonListener {
+
+    public static int MAX_LENGTH = 5;
 
     public PrefixCommand(Locale locale, String prefix) {
         super(locale, prefix);
@@ -26,25 +38,51 @@ public class PrefixCommand extends Command {
     @Override
     public boolean onTrigger(@NotNull CommandEvent event, @NotNull String args) {
         if (args.length() > 0) {
-            if (args.length() <= 5) {
+            if (args.length() <= MAX_LENGTH) {
                 Prefix.changePrefix(event.getGuild(), getLocale(), args);
-                drawMessageNew(EmbedFactory.getEmbedDefault(this, getString("changed", args)))
+                drawMessageNew(EmbedFactory.getEmbedDefault(this, getString("changed", StringUtil.escapeMarkdownInField(args))))
                         .exceptionally(ExceptionLogger.get());
                 return true;
             } else {
                 drawMessageNew(EmbedFactory.getEmbedError(
                         this,
-                        TextManager.getString(getLocale(), TextManager.GENERAL, "args_too_long", "5")
+                        TextManager.getString(getLocale(), TextManager.GENERAL, "args_too_long", StringUtil.numToString(MAX_LENGTH))
                 )).exceptionally(ExceptionLogger.get());
                 return false;
             }
         } else {
-            drawMessageNew(EmbedFactory.getEmbedError(
-                    this,
-                    getString("no_arg")
-            )).exceptionally(ExceptionLogger.get());
+            registerButtonListener(event.getMember());
             return false;
         }
+    }
+
+    @Override
+    public boolean onButton(@NotNull ButtonInteractionEvent event) throws Throwable {
+        String prefix = DBGuild.getInstance().retrieve(event.getGuild().getIdLong()).getPrefix();
+        TextInput textInput = TextInput.create("text", getString("new"), TextInputStyle.SHORT)
+                .setValue(prefix)
+                .setMinLength(1)
+                .setMaxLength(MAX_LENGTH)
+                .build();
+
+        Modal modal = ModalMediator.createModal(getString("button"), e -> {
+                    deregisterListeners();
+                    String newPrefix = e.getValues().get(0).getAsString();
+                    Prefix.changePrefix(event.getGuild(), getLocale(), newPrefix);
+                    drawMessage(EmbedFactory.getEmbedDefault(this, getString("changed", StringUtil.escapeMarkdownInField(newPrefix))))
+                            .exceptionally(ExceptionLogger.get());
+                    e.deferEdit().queue();
+                }).addActionRow(textInput)
+                .build();
+        event.replyModal(modal).queue();
+        return false;
+    }
+
+    @Override
+    public EmbedBuilder draw(@NotNull Member member) throws Throwable {
+        String prefix = DBGuild.getInstance().retrieve(member.getGuild().getIdLong()).getPrefix();
+        setComponents(getString("button"));
+        return EmbedFactory.getEmbedDefault(this, getString("current", StringUtil.escapeMarkdownInField(prefix)));
     }
 
 }
--- src/main/java/core/ModalMediator.java
+++ src/main/java/core/ModalMediator.java
@@ -4,7 +4,6 @@ import java.time.Duration;
 import java.util.function.Consumer;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-import constants.Settings;
 import core.utils.RandomUtil;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.interactions.components.Modal;
@@ -12,7 +11,7 @@ import net.dv8tion.jda.api.interactions.components.Modal;
 public class ModalMediator {
 
     private static final Cache<String, Consumer<ModalInteractionEvent>> cache = CacheBuilder.newBuilder()
-            .expireAfterWrite(Duration.ofMinutes(Settings.TIME_OUT_MINUTES))
+            .expireAfterWrite(Duration.ofHours(1))
             .build();
 
     public static Modal.Builder createModal(String title, Consumer<ModalInteractionEvent> consumer) {
@@ -22,7 +21,9 @@ public class ModalMediator {
     }
 
     public static Consumer<ModalInteractionEvent> get(String customId) {
-        return cache.getIfPresent(customId);
+        Consumer<ModalInteractionEvent> consumer = cache.getIfPresent(customId);
+        cache.invalidate(customId);
+        return consumer;
     }
 
 }
