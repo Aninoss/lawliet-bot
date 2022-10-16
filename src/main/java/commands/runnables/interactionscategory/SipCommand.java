c6dfc5492b4112c182fbd26e6414d15922abbb85
Aninoss <leonblech@tutanota.com>
2022-10-16 10:27:34 +0200

    New gif for L.sip

--- src/main/java/commands/runnables/interactionscategory/SipCommand.java
+++ src/main/java/commands/runnables/interactionscategory/SipCommand.java
@@ -31,7 +31,8 @@ public class SipCommand extends RolePlayAbstract {
                 "https://cdn.discordapp.com/attachments/761612990784471041/834837478748979270/sip.gif",
                 "https://cdn.discordapp.com/attachments/761612990784471041/834837495506010152/sip.gif",
                 "https://cdn.discordapp.com/attachments/761612990784471041/834837508943642714/sip.gif",
-                "https://cdn.discordapp.com/attachments/761612990784471041/860571962316292096/sip.gif"
+                "https://cdn.discordapp.com/attachments/761612990784471041/860571962316292096/sip.gif",
+                "https://cdn.discordapp.com/attachments/761612990784471041/1031120861303935116/sip.gif"
         );
     }
 
