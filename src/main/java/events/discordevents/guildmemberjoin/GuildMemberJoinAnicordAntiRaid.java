9354cd846ba779ad2a800d83ee10147a21acf61f
Aninoss <leonblech@tutanota.com>
2022-11-17 15:09:33 +0100

    Anicord anti raid

---
+++ src/main/java/events/discordevents/guildmemberjoin/GuildMemberJoinAnicordAntiRaid.java
@@ -0,0 +1,60 @@
+package events.discordevents.guildmemberjoin;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import constants.AssetIds;
+import core.schedule.MainScheduler;
+import events.discordevents.DiscordEvent;
+import events.discordevents.EventPriority;
+import events.discordevents.eventtypeabstracts.GuildMemberJoinAbstract;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+
+@DiscordEvent(priority = EventPriority.HIGH, allowBots = true, allowBannedUser = true)
+public class GuildMemberJoinAnicordAntiRaid extends GuildMemberJoinAbstract {
+
+    private static boolean lockdown = false;
+    private static final ArrayList<Instant> instantList = new ArrayList<>();
+
+    @Override
+    public boolean onGuildMemberJoin(GuildMemberJoinEvent event) throws Throwable {
+        if (event.getGuild().getIdLong() != AssetIds.ANICORD_SERVER_ID) {
+            return true;
+        }
+        if (lockdown) {
+            return false;
+        }
+
+        synchronized (this) {
+            Instant now = Instant.now();
+            instantList.add(now);
+            if (instantList.size() > 10) {
+                instantList.remove(0);
+
+                if (instantList.get(0).plus(Duration.ofMinutes(1)).isAfter(now)) {
+                    lockdown = true;
+                    event.getGuild().getManager()
+                            .setInvitesDisabled(true)
+                            .reason("Raid Protection")
+                            .queue();
+                    event.getGuild().getTextChannelById(462420339364724751L)
+                            .sendMessage("Raid wurde erkannt! Invites zum Server sind temporär deaktiviert.")
+                            .queue();
+
+                    MainScheduler.schedule(10, ChronoUnit.MINUTES, "anicord_anti_raid", () -> {
+                        event.getGuild().getManager()
+                                .setInvitesDisabled(false)
+                                .reason("Raid Protection")
+                                .queue();
+                        lockdown = false;
+                    });
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+}
