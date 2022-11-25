66b4ccae54c81daa1ebf756a130963bea95dfb25
Aninoss <leonblech@tutanota.com>
2022-11-25 14:45:39 +0100

    Rule34 hentai abstract improved

--- src/main/java/commands/runnables/Rule34HentaiAbstract.java
+++ src/main/java/commands/runnables/Rule34HentaiAbstract.java
@@ -13,7 +13,9 @@ public abstract class Rule34HentaiAbstract extends PornPredefinedAbstract {
     protected Set<String> getAdditionalFilters() {
         return Set.of("human_on_anthro", "interspecies", "furry", "anthro", "pony", "monster", "animal_humanoid",
                 "no_humans", "feral", "knot", "animal_genitalia", "sonic_(series)", "bowser", "yoshi", "horse",
-                "horsecock", "animal_crossing", "the_simpsons", "mammal");
+                "horsecock", "animal_crossing", "the_simpsons", "family_guy", "south_park", "american_dad", "spider-man",
+                "mammal", "dildo", "machine", "electrostimulation", "anilingus", "toy", "facefuck", "pregnant",
+                "cigarette", "cartoony", "gaping", "pee", "peeing", "piss", "pissing");
     }
 
     @Override
