501f334d2b9f574ae807a4f363becaf8c6fc8339
Aninoss <git@aninoss.dev>
2024-02-07 09:11:43 +0100

    Limit error logs per minute

--- src/main/java/core/ExceptionFilter.java
+++ src/main/java/core/ExceptionFilter.java
@@ -1,9 +1,5 @@
 package core;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
-import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.spi.IThrowableProxy;
 import ch.qos.logback.classic.spi.ThrowableProxy;
@@ -11,7 +7,17 @@ import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
 import events.sync.SendEvent;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Objects;
+
 public class ExceptionFilter extends Filter<ILoggingEvent> {
+
+    public static int MAX_ERRORS_PER_MINUTE = Integer.parseInt(Objects.requireNonNullElse(System.getenv("MAX_ERRORS_PER_MINUTE"), "30"));
+
+    private final RatelimitManager ratelimitManager = new RatelimitManager();
 
     @Override
     public FilterReply decide(final ILoggingEvent event) {
@@ -25,11 +31,13 @@ public class ExceptionFilter extends Filter<ILoggingEvent> {
             return FilterReply.NEUTRAL;
         }
 
-        if (event.getLevel() == Level.ERROR) {
-            String message = throwableProxy != null
-                    ? event.getFormattedMessage() + "\n" + extractMessageAndStackTrace(throwableProxy)
-                    : event.getFormattedMessage();
-            SendEvent.sendException(message);
+        String message = throwableProxy != null
+                ? event.getFormattedMessage() + "\n" + extractMessageAndStackTrace(throwableProxy)
+                : event.getFormattedMessage();
+        SendEvent.sendException(message);
+
+        if (ratelimitManager.checkAndSet(0L, MAX_ERRORS_PER_MINUTE, Duration.ofMinutes(1)).isPresent()) {
+            return FilterReply.DENY;
         }
 
         return FilterReply.NEUTRAL;
