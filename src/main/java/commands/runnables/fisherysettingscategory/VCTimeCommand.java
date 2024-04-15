ad6be3415c93110cc89866a4c5a8a749947186e5
Aninoss <git@aninoss.dev>
2024-04-15 10:58:24 +0200

    L.vctime command improvements

--- src/main/java/commands/runnables/fisherysettingscategory/VCTimeCommand.java
+++ src/main/java/commands/runnables/fisherysettingscategory/VCTimeCommand.java
@@ -3,12 +3,11 @@ package commands.runnables.fisherysettingscategory;
 import commands.Command;
 import commands.CommandEvent;
 import commands.listeners.CommandProperties;
-import commands.listeners.MessageInputResponse;
 import commands.listeners.OnButtonListener;
-import commands.listeners.OnMessageInputListener;
+import constants.LogStatus;
 import core.EmbedFactory;
-import core.ExceptionLogger;
 import core.TextManager;
+import core.modals.ModalMediator;
 import core.utils.StringUtil;
 import mysql.hibernate.entity.BotLogEntity;
 import mysql.hibernate.entity.guild.FisheryEntity;
@@ -16,9 +15,11 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle;
+import net.dv8tion.jda.api.interactions.components.text.TextInput;
+import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
+import net.dv8tion.jda.api.interactions.modals.Modal;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Locale;
@@ -29,14 +30,16 @@ import java.util.Locale;
         emoji = "⏲️",
         executableWithoutArgs = true,
         patreonRequired = true,
-        aliases = { "voicechanneltime", "vccap", "voicechannelcap", "vccaps", "vclimit", "vclimits", "vctimeout" }
+        aliases = {"voicechanneltime", "vccap", "voicechannelcap", "vccaps", "vclimit", "vclimits", "vctimeout"}
 )
-public class VCTimeCommand extends Command implements OnButtonListener, OnMessageInputListener {
+public class VCTimeCommand extends Command implements OnButtonListener {
 
+    private static final String BUTTON_ID_SET_LIMIT = "set";
     private static final String BUTTON_ID_UNLIMITED = "unlimited";
     private static final String BUTTON_ID_CANCEL = "cancel";
 
     private EmbedBuilder eb;
+    private boolean completed = false;
 
     public VCTimeCommand(Locale locale, String prefix) {
         super(locale, prefix);
@@ -44,62 +47,93 @@ public class VCTimeCommand extends Command implements OnButtonListener, OnMessag
 
     @Override
     public boolean onTrigger(@NotNull CommandEvent event, @NotNull String args) {
-        if (!args.isEmpty()) {
-            drawMessage(mainExecution(args, event.getMember())).exceptionally(ExceptionLogger.get());
-        } else {
-            int voiceHoursLimitEffectively = getGuildEntity().getFishery().getVoiceHoursLimitEffectively();
-            this.eb = EmbedFactory.getEmbedDefault(
-                    this,
-                    getString(
-                            "status",
-                            voiceHoursLimitEffectively != 24,
-                            voiceHoursLimitEffectively != 24
-                                    ? StringUtil.numToString(voiceHoursLimitEffectively)
-                                    : getString("unlimited")
-                    )
-            );
+        int voiceHoursLimitEffectively = getGuildEntity().getFishery().getVoiceHoursLimitEffectively();
+        this.eb = EmbedFactory.getEmbedDefault(
+                this,
+                getString(
+                        "status",
+                        voiceHoursLimitEffectively != 24,
+                        voiceHoursLimitEffectively != 24
+                                ? StringUtil.numToString(voiceHoursLimitEffectively)
+                                : getString("unlimited")
+                )
+        );
 
-            setComponents(
-                    Button.of(ButtonStyle.PRIMARY, BUTTON_ID_UNLIMITED, getString("setunlimited")),
-                    Button.of(ButtonStyle.SECONDARY, BUTTON_ID_CANCEL, TextManager.getString(getLocale(), TextManager.GENERAL, "process_abort"))
-            );
-            registerButtonListener(event.getMember());
-            registerMessageInputListener(event.getMember(), false);
-        }
+        registerButtonListener(event.getMember());
         return true;
     }
 
-    private EmbedBuilder mainExecution(String args, Member member) {
-        if (args.equalsIgnoreCase("unlimited")) {
-            return markUnlimited(member);
+    @Override
+    public boolean onButton(@NotNull ButtonInteractionEvent event) throws Throwable {
+        switch (event.getComponentId()) {
+            case BUTTON_ID_UNLIMITED -> {
+                deregisterListenersWithComponents();
+
+                FisheryEntity fishery = getGuildEntity().getFishery();
+                fishery.beginTransaction();
+                BotLogEntity.log(getEntityManager(), BotLogEntity.Event.FISHERY_VOICE_HOURS_LIMIT, event.getMember(), fishery.getVoiceHoursLimit(), 24);
+                fishery.setVoiceHoursLimit(24);
+                fishery.commitTransaction();
+
+                completed = true;
+                this.eb = EmbedFactory.getEmbedDefault(this, getString("success", getNumberSlot(null), getString("unlimited")));
+                return true;
+            }
+            case BUTTON_ID_SET_LIMIT -> {
+                String ID = "text";
+                int currentLimit = getGuildEntity().getFishery().getVoiceHoursLimitEffectively();
+                TextInput textInput = TextInput.create(ID, getString("hoursperday"), TextInputStyle.SHORT)
+                        .setRequiredRange(1, 2)
+                        .setValue(currentLimit < 24 ? String.valueOf(currentLimit) : null)
+                        .build();
+
+                Modal modal = ModalMediator.createDrawableCommandModal(this, getString("setlimit"), e -> {
+                            String input = e.getValue(ID).getAsString();
+
+                            if (!StringUtil.stringIsInt(input)) {
+                                setLog(LogStatus.FAILURE, TextManager.getString(getLocale(), TextManager.GENERAL, "no_digit"));
+                                return null;
+                            }
+
+                            int value = Integer.parseInt(input);
+                            if (value < 1 || value > 23) {
+                                setLog(LogStatus.FAILURE, TextManager.getString(getLocale(), TextManager.GENERAL, "number", "1", "23"));
+                                return null;
+                            }
+
+                            FisheryEntity fishery = getGuildEntity().getFishery();
+                            fishery.beginTransaction();
+                            BotLogEntity.log(getEntityManager(), BotLogEntity.Event.FISHERY_VOICE_HOURS_LIMIT, e.getMember(), fishery.getVoiceHoursLimit(), value);
+                            fishery.setVoiceHoursLimit(value);
+                            fishery.commitTransaction();
+
+                            completed = true;
+                            return EmbedFactory.getEmbedDefault(this, getString("success", getNumberSlot(value), StringUtil.numToString(value)));
+                        })
+                        .addActionRow(textInput)
+                        .build();
+
+                event.replyModal(modal).queue();
+                return false;
+            }
+            case BUTTON_ID_CANCEL -> {
+                deregisterListenersWithComponentMessage();
+                return false;
+            }
         }
-
-        if (!StringUtil.stringIsInt(args)) {
-            return EmbedFactory.getEmbedError(this, TextManager.getString(getLocale(), TextManager.GENERAL, "no_digit"));
-        }
-
-        int value = Integer.parseInt(args);
-        if (value < 1 || value > 23) {
-            return EmbedFactory.getEmbedError(this, TextManager.getString(getLocale(), TextManager.GENERAL, "number", "1", "23"));
-        }
-
-        FisheryEntity fishery = getGuildEntity().getFishery();
-        fishery.beginTransaction();
-        BotLogEntity.log(getEntityManager(), BotLogEntity.Event.FISHERY_VOICE_HOURS_LIMIT, member, fishery.getVoiceHoursLimit(), value);
-        fishery.setVoiceHoursLimit(value);
-        fishery.commitTransaction();
-
-        return EmbedFactory.getEmbedDefault(this, getString("success", getNumberSlot(value), StringUtil.numToString(value)));
+        return false;
     }
 
-    private EmbedBuilder markUnlimited(Member member) {
-        FisheryEntity fishery = getGuildEntity().getFishery();
-        fishery.beginTransaction();
-        BotLogEntity.log(getEntityManager(), BotLogEntity.Event.FISHERY_VOICE_HOURS_LIMIT, member, fishery.getVoiceHoursLimit(), 24);
-        fishery.setVoiceHoursLimit(24);
-        fishery.commitTransaction();
-
-        return EmbedFactory.getEmbedDefault(this, getString("success", getNumberSlot(null), getString("unlimited")));
+    @Override
+    public EmbedBuilder draw(@NotNull Member member) throws Throwable {
+        if (!completed) {
+            setComponents(
+                    Button.of(ButtonStyle.PRIMARY, BUTTON_ID_SET_LIMIT, getString("setlimit")),
+                    Button.of(ButtonStyle.PRIMARY, BUTTON_ID_UNLIMITED, getString("setunlimited")),
+                    Button.of(ButtonStyle.SECONDARY, BUTTON_ID_CANCEL, TextManager.getString(getLocale(), TextManager.GENERAL, "process_abort"))
+            );
+        }
+        return new EmbedBuilder(this.eb);
     }
 
     private int getNumberSlot(Integer i) {
@@ -107,31 +141,6 @@ public class VCTimeCommand extends Command implements OnButtonListener, OnMessag
             return 0;
         } else if (i == 1) return 1;
         return 2;
-    }
-
-    @Override
-    public MessageInputResponse onMessageInput(@NotNull MessageReceivedEvent event, @NotNull String input) throws Throwable {
-        deregisterListenersWithComponents();
-        this.eb = mainExecution(input, event.getMember());
-        return MessageInputResponse.SUCCESS;
-    }
-
-    @Override
-    public boolean onButton(@NotNull ButtonInteractionEvent event) throws Throwable {
-        if (event.getComponentId().equals(BUTTON_ID_UNLIMITED)) {
-            deregisterListenersWithComponents();
-            this.eb = markUnlimited(event.getMember());
-            return true;
-        } else if (event.getComponentId().equals(BUTTON_ID_CANCEL)) {
-            deregisterListenersWithComponentMessage();
-            return false;
-        }
-        return false;
-    }
-
-    @Override
-    public EmbedBuilder draw(@NotNull Member member) throws Throwable {
-        return this.eb;
     }
 
 }
--- src/main/java/core/modals/ModalMediator.java
+++ src/main/java/core/modals/ModalMediator.java
@@ -5,19 +5,14 @@ import com.google.common.cache.CacheBuilder;
 import commands.Command;
 import commands.listeners.Drawable;
 import commands.runnables.NavigationAbstract;
-import constants.LogStatus;
 import core.ExceptionLogger;
-import core.TextManager;
-import core.utils.*;
+import core.utils.ExceptionUtil;
+import core.utils.RandomUtil;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
-import net.dv8tion.jda.api.interactions.components.text.TextInput;
-import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import net.dv8tion.jda.api.interactions.modals.Modal;
-import net.dv8tion.jda.api.interactions.modals.ModalMapping;
 
 import java.time.Duration;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class ModalMediator {
