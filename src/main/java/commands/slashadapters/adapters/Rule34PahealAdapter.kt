83747ea76a153b5a755c9fe2a20ad3a0c20371b8
Aninoss <git@aninoss.dev>
2024-04-18 09:22:18 +0200

    L.r34p removed

--- src/main/java/commands/slashadapters/adapters/Rule34PahealAdapter.kt
+++
@@ -1,7 +0,0 @@
-package commands.slashadapters.adapters
-
-import commands.runnables.nsfwcategory.Rule34PahealCommand
-import commands.slashadapters.Slash
-
-@Slash(command = Rule34PahealCommand::class)
-class Rule34PahealAdapter : BooruSearchAdapterAbstract()
