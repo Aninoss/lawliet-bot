fbb6bbb3a8c53d0f25243f63e36be12b4abc61fb
Aninoss <git@aninoss.dev>
2025-01-07 13:49:14 +0100

    Fix birthday list leap year bug

--- src/main/java/mysql/hibernate/entity/guild/BirthdayUserEntryEntity.kt
+++ src/main/java/mysql/hibernate/entity/guild/BirthdayUserEntryEntity.kt
@@ -18,11 +18,8 @@ class BirthdayUserEntryEntity {
 
     fun isBirthday(): Boolean {
         val now = ZonedDateTime.now(ZoneId.of(timeZoneEffectively))
-        val moveDateDueToNonLeapYear = day == 29 && month == 2 && YearMonth.of(now.year, 2).lengthOfMonth() == 28
-        val adjustedDay = if (moveDateDueToNonLeapYear) 1 else day
-        val adjustedMonth = if (moveDateDueToNonLeapYear) 3 else month
-
-        return now.dayOfMonth == adjustedDay && now.month.value == adjustedMonth && (triggerYear == null || now.year >= triggerYear!!)
+        val date = getAdjustedDate(now.year)
+        return now.dayOfMonth == date?.dayOfMonth && now.monthValue == date.monthValue && (triggerYear == null || now.year >= triggerYear!!)
     }
 
     fun updateTriggerYear() {
@@ -36,13 +33,26 @@ class BirthdayUserEntryEntity {
 
         val zoneId = ZoneId.of(timeZoneEffectively)
         val now = ZonedDateTime.now(ZoneId.of(timeZoneEffectively))
+        val date = getAdjustedDate(now.year)!!
 
-        val instant = LocalDate.of(now.year, month!!, day!!).atStartOfDay(zoneId).toInstant()
+        val instant = LocalDate.of(now.year, date.monthValue, date.dayOfMonth).atStartOfDay(zoneId).toInstant()
         if (instant.plus(Duration.ofDays(1)).isBefore(Instant.now())) {
-            return LocalDate.of(now.year + 1, month!!, day!!).atStartOfDay(zoneId).toInstant()
+            val newDate = getAdjustedDate(now.year + 1)!!
+            return LocalDate.of(now.year + 1, newDate.monthValue, newDate.dayOfMonth).atStartOfDay(zoneId).toInstant()
         } else {
             return instant
         }
+    }
+
+    private fun getAdjustedDate(year: Int): MonthDay? {
+        if (month == null || day == null) {
+            return null
+        }
+
+        val moveDateDueToNonLeapYear = day == 29 && month == 2 && YearMonth.of(year, 2).lengthOfMonth() == 28
+        val adjustedDay = if (moveDateDueToNonLeapYear) 1 else day!!
+        val adjustedMonth = if (moveDateDueToNonLeapYear) 3 else month!!
+        return MonthDay.of(adjustedMonth, adjustedDay)
     }
 
 }
