9e497e3ee7365c275f34a1324311a71b622fa20c
Aninoss <git@aninoss.dev>
2025-01-19 18:52:14 +0100

    Improve timeout in http client

--- src/main/java/core/internet/HttpClient.java
+++ src/main/java/core/internet/HttpClient.java
@@ -1,15 +1,14 @@
 package core.internet;
 
-import java.util.concurrent.TimeUnit;
 import net.dv8tion.jda.internal.utils.IOUtil;
 import okhttp3.OkHttpClient;
+
+import java.util.concurrent.TimeUnit;
 
 public class HttpClient {
 
     private static final OkHttpClient client = IOUtil.newHttpClientBuilder()
-            .connectTimeout(10, TimeUnit.SECONDS)
-            .writeTimeout(10, TimeUnit.SECONDS)
-            .readTimeout(10, TimeUnit.SECONDS)
+            .callTimeout(10, TimeUnit.SECONDS)
             .build();
 
     public static OkHttpClient getClient() {
